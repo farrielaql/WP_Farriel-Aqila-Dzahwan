f62a87bdd3c170af3e20a05a6fc38498f333f7e3
farrielaql <farriel2015234@gmail.com>
2024-04-05 09:55:40 +0700

    view-form-matakuliah

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -5,7 +5,7 @@
 
 <body>
     <center>
-         <form action="<?= base_url('matakuliah/cetak'); ?>"
+         <form  action="<?=     base_url('matakuliah/cetak'); ?>"
 method="post">
             <table>
                 <tr>
